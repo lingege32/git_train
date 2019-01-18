1db6fa7377d5ba4ca6f361ac68b728121d0ebf98
lingege32 <lingege32@gmail.com>
2019-01-18 15:26:19 +0800

    develop branch init

--- src/test.cpp
+++ src/test.cpp
@@ -6,6 +6,11 @@
 
 int main(int argc, char *argv[])
 {
+	if(argc!=1)
+	{
+		std::cout<<"	\e[39;38;5;2merror! no parameter\e[39;38;0;2m\n";
+		return 0;
+	}
 	std::vector<int> vTest{1,2,3,4,5,6,7,8,9};
 	std::sort(std::begin(vTest),std::end(vTest),std::greater<int>());
 	std::copy(std::begin(vTest),std::end(vTest),std::ostream_iterator<int>(std::cout,"\t"));
